637a39922d626287efb3676440423d62996d2e59
NikitaKol <nikitak@mail.fresnostate.edu>
2021-11-08 10:59:02 +0100

    Created scheduling system for SibRus.

---
+++ SibRus/Systems/SchedulingSystem.cs
@@ -0,0 +1,80 @@
+﻿using SibRus.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SibRus.Systems
+{
+    public class SchedulingSystem
+    {
+        private int _time;
+        private readonly SortedDictionary<int, List<IScheduleable>> _scheduleables;
+
+        public SchedulingSystem()
+        {
+            _time = 0;
+            _scheduleables = new SortedDictionary<int, List<IScheduleable>>();
+        }
+
+        public void Add( IScheduleable scheduleable )
+        {
+            int key = _time + scheduleable.Time;
+            if (!_scheduleables.ContainsKey(key))
+            {
+                _scheduleables.Add(key, new List<IScheduleable>());
+            }
+            _scheduleables[key].Add(scheduleable);
+        }
+
+        public void Remove( IScheduleable scheduleable)
+        {
+            KeyValuePair<int, List<IScheduleable>> scheduleableListFound
+                =new KeyValuePair<int, List<IScheduleable>>(-1, null);
+
+            foreach (var scheduleablesList in _scheduleables)
+            {
+                if (scheduleablesList.Value.Contains(scheduleable))
+                {
+                    scheduleableListFound = scheduleablesList;
+                    break;
+                }
+            }
+
+            if (scheduleableListFound.Value != null)
+            {
+                scheduleableListFound.Value.Remove(scheduleable);
+                if(scheduleableListFound.Value.Count <= 0)
+                {
+                    _scheduleables.Remove(scheduleableListFound.Key);
+                }
+            }
+        }
+
+        // Get the next object whose turn it is from the schedule. Advance time if necessary
+        public IScheduleable Get()
+        {
+            var firstScheduleableGroup = _scheduleables.First();
+            var firstScheduleable = firstScheduleableGroup.Value.First();
+
+            Remove(firstScheduleable);
+            _time = firstScheduleableGroup.Key;
+
+            return firstScheduleable;
+        }
+
+        // Get the current time (turn) for the schedule
+        public int GetTime()
+        {
+            return _time;
+        }
+
+        //To reset time and clear out schedule.
+        public void Clear()
+        {
+            _time = 0;
+            _scheduleables.Clear();
+        }
+    }
+}
